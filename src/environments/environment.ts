da562250e94a3f881d03f875d1f44aa05b6a0605
clinicap247 <clinicap247@gmail.com>
2024-11-11 06:07:21 -0400

    Azure endpoint url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production : true,
-  api_url : 'https://f9fa-179-60-117-159.ngrok-free.app/graphql',
+  api_url : 'https://clinicapp-drbva9enacewftf0.brazilsouth-01.azurewebsites.net/graphql',
   identity_url : 'https://tly1a3n3b7.execute-api.us-east-1.amazonaws.com',
 };
